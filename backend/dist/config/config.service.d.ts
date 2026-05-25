bb78f72636ef347d4ec678762e5bb30e7ba99e21
Khoa Nguyen <yunkhngn.mail@gmail.com>
2026-05-25 19:33:16 +0700

    chore: remove dist from tracking and add to gitignore

--- backend/dist/config/config.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { ConfigService as NestConfigService } from '@nestjs/config';
-export declare class AppConfigService {
-    private configService;
-    constructor(configService: NestConfigService);
-    get port(): number;
-    get nodeEnv(): string;
-    get databaseUrl(): string;
-    get firebaseServiceAccount(): string;
-}
