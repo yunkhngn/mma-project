e31053acc82d90760156ae79fa41ac80c370fc1a
quangoku <lequan159753@gmail.com>
2026-05-30 10:02:45 +0700

    reinit mobile

--- mobile/src/components/templates/index.ts
+++
@@ -1,2 +0,0 @@
-export { AuthScreenTemplate } from './AuthScreenTemplate';
-
--- mobile/src/types/declarations.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.css';
-declare module '*.module.css';
-
-declare module 'firebase/auth/react-native' {
-    import { Persistence } from 'firebase/auth';
-    export function getReactNativePersistence(storage: any): Persistence;
-}
