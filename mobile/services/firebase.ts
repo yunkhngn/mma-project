242c51e462c06ad4fd673c22859f35eb40712d82
Khoa Nguyen <yunkhngn.mail@gmail.com>
2026-06-02 23:46:00 +0700

    fix: resolve firebase auth react-native resolver issue in metro

--- mobile/services/firebase.ts
+++ mobile/services/firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
-import { initializeAuth } from 'firebase/auth';
 // @ts-ignore
-import { getReactNativePersistence } from 'firebase/auth/react-native';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
